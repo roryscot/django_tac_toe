81e778bee5e9fd4a00bdab4978e351026f5b25ad
Rory Dahl <rsdah13@gmail.com>
2017-12-16 17:48:58 -0800

    Django Templates.

--- tictactoe/tictactoe/settings.py
+++ tictactoe/tictactoe/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'gameplay'
+    'gameplay',
+    'player',
 ]
 
 MIDDLEWARE = [
@@ -63,7 +64,6 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
-                'gameplay',
             ],
         },
     },
