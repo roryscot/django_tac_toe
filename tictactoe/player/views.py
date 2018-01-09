fe4a1d0d28ac1e2273ac2293767c533e5ff64ce3
Rory Dahl <rsdah13@gmail.com>
2018-01-08 16:00:31 -0800

    Hamdlin Form Submission.

--- tictactoe/player/views.py
+++ tictactoe/player/views.py
@@ -1,8 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 
 from gameplay.models import Game
 from .forms import InvitationForm
+from .models import Invitation
 
 @login_required
 def home (request):
@@ -14,5 +15,13 @@ def home (request):
 
 @login_required
 def new_invitation(request):
-    form = InvitationForm()
+    if request.method == 'POST':
+        # validate submission
+        invitation = Invitation(from_user=request.user)
+        form = InvitationForm(instance=invitation, data=request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('player_home')
+    else:
+        form = InvitationForm()
     return render(request, "player/new_invitation_form.html", {"form": form})
